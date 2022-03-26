d804dd22a371b44183f51d4a7340e33070fe270f
quan_developer <quan.ios.developer@gmail.com>
2022-03-26 12:02:25 +0800

    modify DioRequest

--- lib/src/dio_request.dart
+++ lib/src/dio_request.dart
@@ -5,99 +5,126 @@ import 'package:dio/dio.dart';
 import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
 
 class DioRequest with DioMixin {
   ///缓存配置
-  late CacheOptions cacheOptions;
+  late CacheOptions? cacheOptions;
 
   ///Cookie 配置
-  late CookieJar cookieJar;
+  CookieJar? cookieJar;
 
+  ///构造方法
+  ///如果baseUrl不等于空，options中的baseUrl属性会被这个参数覆盖
   DioRequest(
       {String? baseUrl,
       BaseOptions? op,
       LogInterceptor? log,
       CacheOptions? cache,
-      CookieJar? cookie,
+      String? cookiePath,
       List<Interceptor>? interceptorList}) {
     options = op ?? DefaultOption(baseUrl: baseUrl);
-    if (kDebugMode) {
-      final logInterceptor = log ??
-          LogInterceptor(
-              responseBody: true,
-              error: true,
-              requestHeader: false,
-              responseHeader: false,
-              request: false,
-              requestBody: true);
-      interceptors.add(logInterceptor);
+    if (baseUrl != null && baseUrl.isNotEmpty) {
+      options.baseUrl = baseUrl;
     }
-    cacheOptions = cache ??
-        CacheOptions(
-          store: MemCacheStore(),
-          hitCacheOnErrorExcept: [401, 403, 404],
-          maxStale: const Duration(days: 7),
-        );
-    interceptors.add(DioCacheInterceptor(options: cacheOptions));
-    cookieJar = cookie ?? PersistCookieJar(ignoreExpires: true);
-    interceptors.add(CookieManager(cookieJar));
+    if (kDebugMode && log != null) {
+      interceptors.add(log);
+    }
+    if (cache != null) {
+      cacheOptions = cache;
+      interceptors.add(DioCacheInterceptor(options: cache));
+    }
+    if (cookiePath != null && cookiePath.isNotEmpty) {
+      cookieJar = PersistCookieJar(storage: FileStorage(cookiePath));
+      interceptors.add(CookieManager(cookieJar!));
+    }
     if (interceptorList != null && interceptorList.isNotEmpty) {
       interceptors.addAll(interceptorList);
     }
   }
 
+  ///创建默认的DioRequest实例
+  static Future<DioRequest> getInstance(
+      {String? baseUrl,
+      BaseOptions? op,
+      List<Interceptor>? interceptorList}) async {
+    final logInterceptor = LogInterceptor(
+        responseBody: true,
+        error: true,
+        requestHeader: false,
+        responseHeader: false,
+        request: false,
+        requestBody: true);
+
+    final cacheOptions = CacheOptions(
+        store: MemCacheStore(),
+        hitCacheOnErrorExcept: [401, 403, 404],
+        maxStale: const Duration(days: 7));
+
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String cookiePath = appDocDir.path + "/.cookies/";
+
+    return DioRequest(
+        baseUrl: baseUrl,
+        op: op,
+        log: logInterceptor,
+        cache: cacheOptions,
+        cookiePath: cookiePath,
+        interceptorList: interceptorList);
+  }
+
   ///判断指定key的缓存是否存在
   Future<bool> cacheExists(String key) async {
-    return await cacheOptions.store?.exists(key) ?? false;
+    return await cacheOptions?.store?.exists(key) ?? false;
   }
 
   ///获取指定Key的缓存
   Future<CacheResponse?> getCache(String key) async {
-    return await cacheOptions.store?.get(key);
+    return await cacheOptions?.store?.get(key);
   }
 
   ///设置缓存
   Future<void> setCache(CacheResponse response) async {
-    await cacheOptions.store?.set(response);
+    await cacheOptions?.store?.set(response);
   }
 
   ///删除缓存
   Future<void> deleteCache(String key, {bool staleOnly = false}) async {
-    await cacheOptions.store?.delete(key, staleOnly: staleOnly);
+    await cacheOptions?.store?.delete(key, staleOnly: staleOnly);
   }
 
   ///清除所有key的缓存
   Future<void> cleanCache(
       {CachePriority priorityOrBelow = CachePriority.high,
       bool staleOnly = false}) async {
-    await cacheOptions.store
+    await cacheOptions?.store
         ?.clean(priorityOrBelow: priorityOrBelow, staleOnly: staleOnly);
   }
 
   ///释放底层资源
   Future<void> closeCache() async {
-    await cacheOptions.store?.close();
+    await cacheOptions?.store?.close();
   }
 
   ///保存指定 uri 的 cookie。
   Future<void> saveFromResponse(Uri uri, List<Cookie> cookies) async {
-    await cookieJar.saveFromResponse(uri, cookies);
+    await cookieJar?.saveFromResponse(uri, cookies);
   }
 
   ///获取指定 uri 的 cookie。
-  Future<List<Cookie>> loadForRequest(Uri uri) async {
-    return await cookieJar.loadForRequest(uri);
+  Future<List<Cookie>?> loadForRequest(Uri uri) async {
+    return await cookieJar?.loadForRequest(uri);
   }
 
   ///删除所以cookie
   Future deleteAllCookie() async {
-    await cookieJar.deleteAll();
+    await cookieJar?.deleteAll();
   }
 
   ///删除指定Uri的Cookie
   Future<void> deleteCookie(Uri uri,
       [bool withDomainSharedCookie = false]) async {
-    await cookieJar.delete(uri, withDomainSharedCookie);
+    await cookieJar?.delete(uri, withDomainSharedCookie);
   }
 }
 
